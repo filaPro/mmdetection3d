d4b6a759276fedde945d02113ffed7a7eefa7884
root <danrukh@gmail.com>
2023-02-20 07:30:54 +0000

    add segmentor and neck

---
+++ projects/TD3D/td3d/td3d.py
@@ -0,0 +1,172 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+# Adapted from https://github.com/SamsungLabs/td3d/blob/master/mmdet3d/detectors/td3d_instance_segmentor.py # noqa
+from typing import List, Union
+
+try:
+    import MinkowskiEngine as ME
+    from MinkowskiEngine import (
+        SparseTensor, SparseTensorQuantizationMode, TensorField)
+except ImportError:
+    # Please follow getting_started.md to install MinkowskiEngine.
+    ME = SparseTensor = SparseTensorQuantizationMode = TensorField = None
+    pass
+
+import torch
+from torch import Tensor
+
+from mmdet3d.registry import MODELS
+from mmdet3d.utils import ConfigType, OptConfigType, OptMultiConfig
+from mmdet3d.structures.det3d_data_sample import SampleList
+from mmdet3d.models import Base3DDetector
+
+
+@MODELS.register_module()
+class TD3D(Base3DDetector):
+    r"""`TD3D <https://arxiv.org/abs/2302.02871>`_.
+   
+    Args:
+        backbone (dict): Config dict of detector's backbone.
+        neck (dict, optional): Config dict of neck. Defaults to None.
+        bbox_head (dict, optional): Config dict of box head. Defaults to None.
+        train_cfg (dict, optional): Config dict of training hyper-parameters.
+            Defaults to None.
+        test_cfg (dict, optional): Config dict of test hyper-parameters.
+            Defaults to None.
+        data_preprocessor (dict or ConfigDict, optional): The pre-process
+            config of :class:`BaseDataPreprocessor`.  it usually includes,
+                ``pad_size_divisor``, ``pad_value``, ``mean`` and ``std``.
+        init_cfg (dict or ConfigDict, optional): the config to control the
+            initialization. Defaults to None.
+    """
+    def __init__(self,
+                 backbone: ConfigType,
+                 neck: OptConfigType = None,
+                 bbox_head: OptConfigType = None,
+                 train_cfg: OptConfigType = None,
+                 test_cfg: OptConfigType = None,
+                 data_preprocessor: OptConfigType = None,
+                 init_cfg: OptMultiConfig = None) -> None:
+        super(TD3D, self).__init__(
+            data_preprocessor=data_preprocessor, init_cfg=init_cfg)
+        if ME is None:
+            raise ImportError(
+                'Please follow `getting_started.md` to install MinkowskiEngine.`'  # noqa: E501
+            )
+        self.backbone = MODELS.build(backbone)
+        if neck is not None:
+            self.neck = MODELS.build(neck)
+        bbox_head.update(train_cfg=train_cfg)
+        bbox_head.update(test_cfg=test_cfg)
+        self.bbox_head = MODELS.build(bbox_head)
+        self.voxel_size = bbox_head['voxel_size']
+        self.train_cfg = train_cfg
+        self.test_cfg = test_cfg
+    
+    def extract_feat(self, x: SparseTensor) -> List[SparseTensor]:
+        """Extract features from sparse tensor.
+
+        Args:
+            x (SparseTensor): Input sparse tensor.
+        
+        Returns:
+            List[SparseTensor]: Features after backbone and neck.
+        """
+        x = self.backbone(x)
+        if self.with_neck:
+            x = self.neck(x)
+        return x
+
+    def collate(self, points: List[Tensor],
+                quantization_mode: SparseTensorQuantizationMode) \
+                -> TensorField:
+        """Collate batch of points to sparse tensor.
+
+        Args:
+            points (List[Tensor]): Batch of points.
+            quantization_mode (SparseTensorQuantizationMode): Minkowski
+                quantization mode. We use random sample for training
+                and unweighted average for inference.
+        
+        Returns:
+            TensorField: Containing features and coordinates of a
+                sparse tensor.
+        """
+        coordinates, features = ME.utils.batch_sparse_collate(
+            [(p[:, :3] / self.voxel_size, p[:, 3:]) for p in points],
+            dtype=points[0].dtype,
+            device=points[0].device)
+        return ME.TensorField(
+            features=features,
+            coordinates=coordinates,
+            quantization_mode=quantization_mode,
+            minkowski_algorithm=ME.MinkowskiAlgorithm.SPEED_OPTIMIZED,
+            device=points[0].device)
+    
+    def loss(self, batch_inputs_dict: dict, batch_data_samples: SampleList,
+             **kwargs) -> Union[dict, list]:
+        """Calculate losses from a batch of inputs dict and data samples.
+
+        Args:
+            batch_inputs_dict (dict): The model input dict which include
+                `points` key.
+            batch_data_samples (List[:obj:`Det3DDataSample`]): The Data
+                Samples. It includes information such as
+                `gt_instance_3d` and `gt_sem_seg_3d`.
+
+        Returns:
+            dict: A dictionary of loss components.
+        """
+        annotated_points = []
+        for i in range(batch_data_samples):
+            annotated_points.append(torch.cat((
+                batch_inputs_dict['points'][i],
+                batch_data_samples[i].gt_instance_3d.unsqueeze(1),
+                batch_data_samples[i].gt_sem_seg_3d.unsqueeze(1))))
+
+        field = self.collate(
+            annotated_points, ME.SparseTensorQuantizationMode.RANDOM_SUBSAMPLE)
+        x = field.sparse()
+        pts_targets = x.features[:, 3:].round().long()
+        x = ME.SparseTensor(
+            x.features[:, :3],
+            coordinate_map_key=x.coordinate_map_key,
+            coordinate_manager=x.coordinate_manager)
+        x = self.extract_feat(x)
+        losses = self.head.loss(x, pts_targets, batch_data_samples, **kwargs)
+        return losses
+    
+    def predict(self, batch_inputs_dict: dict, batch_data_samples: SampleList,
+                **kwargs) -> SampleList:
+        """Predict results from a batch of inputs and data samples with post-
+        processing.
+
+        Args:
+            batch_inputs_dict (dict): The model input dict which include
+                `points` key.
+            batch_data_samples (List[:obj:`Det3DDataSample`]): The Data
+                Samples. It includes information such as
+                `gt_instance_3d` and `gt_sem_seg_3d`.
+
+        Returns:
+            list[:obj:`Det3DDataSample`]: Detection results of the
+            input samples. Each Det3DDataSample contains 'pred_pts_seg'.
+            And the `pred_pts_seg` contains following keys.
+
+                - instance_scores (Tensor): Classification scores, has a shape
+                    (num_instance, )
+                - instance_labels (Tensor): Labels of instances, has a shape
+                    (num_instances, )
+                - pts_instance_mask (Tensor): Instance mask, has a shape
+                    (num_points, num_instances) of type bool.
+
+        """
+        points = batch_inputs_dict['points']
+        field = self.collate(
+            points, ME.SparseTensorQuantizationMode.UNWEIGHTED_AVERAGE)
+        x = field.sparse()
+        x = self.extract_feat(x)
+        results_list = self.bbox_head.predict(
+            x, field, batch_data_samples, **kwargs)
+        for i, data_sample in enumerate(batch_data_samples):
+            data_sample.pred_instances_3d = results_list[i]
+        return batch_data_samples
---
+++ projects/TD3D/td3d/td3d_neck.py
@@ -0,0 +1,119 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+# Adapted from https://github.com/SamsungLabs/td3d/blob/master/mmdet3d/models/necks/ngfc_neck.py # noqa
+from typing import List, Tuple
+
+try:
+    import MinkowskiEngine as ME
+    from MinkowskiEngine import SparseTensor
+except ImportError:
+    # Please follow getting_started.md to install MinkowskiEngine.
+    ME = SparseTensor = None
+    pass
+
+from mmengine.model import BaseModule
+from torch import nn
+
+from mmdet3d.registry import MODELS
+
+
+@MODELS.register_module()
+class TD3DNeck(BaseModule):
+    r"""Neck of `TD3D <https://arxiv.org/abs/2302.02871>`_.
+
+    Args:
+        in_channels (tuple[int]): Number of channels in input tensors.
+        out_channels (int): Number of channels in all output tensors
+            except first.
+        seg_out_channels (int): Number of channels in the first output tensor.
+    """
+
+    def __init__(self, in_channels: Tuple[int], out_channels: int,
+                 seg_out_channels: int):
+        super(TD3DNeck, self).__init__()
+        self._init_layers(in_channels, out_channels, seg_out_channels)
+
+    def _init_layers(self, in_channels: Tuple[int], out_channels: int,
+                     seg_out_channels: int):
+        """Initialize layers.
+
+        Args:
+            in_channels (tuple[int]): Number of channels in input tensors.
+            out_channels (int): Number of channels in all output tensors
+            except first.
+            seg_out_channels (int): Number of channels in the first output tensor.
+        """
+        for i in range(len(in_channels)):
+            if i > 0:
+                stride = 4 if i == 1 else 2
+                self.add_module(
+                    f'up_block_{i}',
+                    self._make_block(in_channels[i], in_channels[i - 1], True,
+                                     stride))
+            if len(in_channels) - 1 > i > 0:
+                self.add_module(
+                    f'lateral_block_{i}',
+                    self._make_block(in_channels[i], in_channels[i]))
+            n_channels = out_channels if i != 0 else seg_out_channels
+            self.add_module(f'out_block_{i}',
+                            self._make_block(in_channels[i], n_channels))
+
+    def init_weights(self):
+        """Initialize weights."""
+        for m in self.modules():
+            if isinstance(m, ME.MinkowskiConvolution):
+                ME.utils.kaiming_normal_(
+                    m.kernel, mode='fan_out', nonlinearity='relu')
+
+            if isinstance(m, ME.MinkowskiBatchNorm):
+                nn.init.constant_(m.bn.weight, 1)
+                nn.init.constant_(m.bn.bias, 0)
+
+    def forward(self, x: List[SparseTensor]) -> List[SparseTensor]:
+        """Forward pass.
+
+        Args:
+            x (list[SparseTensor]): Features from the backbone.
+
+        Returns:
+            List[Tensor]: Output features from the neck.
+        """
+        outs = []
+        inputs = x
+        x = inputs[-1]
+        for i in range(len(inputs) - 1, -1, -1):
+            if i < len(inputs) - 1:
+                x = self.__getattr__(f'up_block_{i + 1}')(x)
+                x = inputs[i] + x
+                if i > 0:
+                    x = self.__getattr__(f'lateral_block_{i}')(x)
+            out = self.__getattr__(f'out_block_{i}')(x)
+            outs.append(out)
+        return outs[::-1]
+
+    @staticmethod
+    def _make_block(in_channels: int,
+                    out_channels: int,
+                    transpose: bool = False,
+                    stride: int = 1) -> nn.Module:
+        """Construct Conv-Norm-Act block.
+
+        Args:
+            in_channels (int): Number of input channels.
+            out_channels (int): Number of output channels.
+            generative (bool): Use transposed convolution if True.
+                Defaults to False.
+            stride (int): Stride of the convolution. Defaults to 1.
+
+        Returns:
+            torch.nn.Module: With corresponding layers.
+        """
+        conv = ME.MinkowskiConvolutionTranspose if transpose \
+            else ME.MinkowskiConvolution
+        return nn.Sequential(
+            conv(
+                in_channels,
+                out_channels,
+                kernel_size=3,
+                stride=stride,
+                dimension=3), ME.MinkowskiBatchNorm(out_channels),
+            ME.MinkowskiReLU(inplace=True))
